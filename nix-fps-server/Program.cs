da8f406cdc6aeece7aa2d7ff7a00997cc9b64e1b
Leandro Osuna <leandrog.osuna@gmail.com>
2024-11-22 02:28:20 -0300

    network loop stable up to 1000 TPS

--- nix-fps-server/Program.cs
+++ nix-fps-server/Program.cs
@@ -2,7 +2,7 @@
 using Newtonsoft.Json.Linq;
 using Riptide;
 using Riptide.Utils;
-using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace nix_fps_server
 {
@@ -13,11 +13,19 @@ namespace nix_fps_server
 
         public static uint ServerTPS;
         public static JObject CFG;
-        public static long syncErrorMs = 0;
+
+
+        private delegate void TimerCallback(uint id, uint msg, IntPtr user, IntPtr param1, IntPtr param2);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        private static extern uint timeSetEvent(uint msDelay, uint msResolution, TimerCallback callback, IntPtr user, uint eventType);
+
+        private static uint timerId;
+        static uint TargetMS;
         static void Main(string[] args)
         {
             CFG = JObject.Parse(File.ReadAllText("app-settings.json"));
-
+            
             RiptideLogger.Initialize(Console.WriteLine, false);
             networkManager = new NetworkManager();
             Server = new Server();
@@ -32,36 +40,42 @@ namespace nix_fps_server
                 File.WriteAllText("app-settings.json", CFG.ToString());
             }
             ServerTPS = CFG["ServerTPS"].Value<uint>();
-            var targetms = (1000 / ServerTPS);
-            
-            
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            while (true) {
-                var ms = stopwatch.ElapsedMilliseconds;
-                var currentTargetMs = targetms - syncErrorMs;
-                if (ms >= currentTargetMs) 
-                {
-                    syncErrorMs = (ms - currentTargetMs);
-                    ServerUpdate();
-                    stopwatch.Restart();
-                }
-                Thread.Sleep(1);
-                 
-            };
+            TargetMS = (1000 / ServerTPS);
+
+            TimerCallback callback = TimerElapsed;
+
+            timerId = timeSetEvent(TargetMS, 0, callback, IntPtr.Zero, 1);
+            Console.WriteLine("Server started. Press Enter to exit.");
+            Console.ReadLine();
+
+        }
+        private static void TimerElapsed(uint id, uint msg, IntPtr user, IntPtr param1, IntPtr param2)
+        {
+            ServerUpdate();
         }
 
         static int t = 0;
+        static DateTime lastDt = DateTime.UtcNow;
         private static void ServerUpdate()
         {
             Server.Update();
 
             networkManager.Update();
-            t++;
 
+            t++;
             if (t == ServerTPS) //1 sec 
             {
                 t = 0;
+                var dt = DateTime.UtcNow;
+                var diff = dt - lastDt;
+                var tps = 1000 / diff.TotalMilliseconds * ServerTPS;
+                var perc = 100000 / diff.TotalMilliseconds;
+
+                //Console.WriteLine($"{dt:HH:mm:ss.fff} [TPS = {tps:###.##} : {perc:###.##}%]" );
+                Console.WriteLine($"{dt:HH:mm:ss.fff} [TPS = {Math.Round(tps)} : {Math.Round(perc)}%]" );
+
+                lastDt = dt;
+
                 //Console.Clear();
                 //networkManager.ShowStatus();
                 networkManager.ShowPacketCount();
